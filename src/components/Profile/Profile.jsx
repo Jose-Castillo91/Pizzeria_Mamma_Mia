13dff7169816e5bc87c6075e7d45dab3a2e5ff03
José Ignacio Castillo Farias <castillofarias@gmail.com>
2025-12-04 19:51:51 -0300

    cambios profile

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,24 +1,31 @@
-import "./Profile.css";
+
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
 function Profile() {
-  const email = "usuario@ejemplo.com";
 
-  const handleLogout = () => {
-    console.log("Cerrar sesión sin funcionalidad por ahora)");
-  };
 
   return (
-    <div className="profile-container">
-      <h2  className="profile-title">Perfil de Usuario</h2>
+    <Form>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control type="email" placeholder="Enter email" />
+        <Form.Text className="text-muted">
+          Well never share your email with anyone else.
+        </Form.Text>
+      </Form.Group>
 
-      <p className="profile-email">
-        <strong>Email:</strong> {email}
-      </p>
-
-      <button className="profile-button" onClick={handleLogout}>
-        Cerrar sesión
-      </button>
-    </div>
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control type="password" placeholder="Password" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicCheckbox">
+        <Form.Check type="checkbox" label="Check me out" />
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
   );
 }
 
